b667a596703a8c3fb90443f23829b0556dd93890
OHarilal <78577011+OHarilal@users.noreply.github.com>
2022-05-01 11:24:33 -0400

    5/1

--- solution.py
+++ solution.py
@@ -6,9 +6,10 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     endmsg = "\r\n.\r\n"
 
     # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-
+    mailserver = "smtp.gmail.com"
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
-
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect(mailserver,port)
     # Fill in start
     # Fill in end
 
@@ -18,7 +19,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     #    print('220 reply not received from server.')
 
     # Send HELO command and print server response.
-    heloCommand = 'HELO Alice\r\n'
+    heloCommand = "HELO Alice\r\n"
     clientSocket.send(heloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
     #print(recv1) 
@@ -27,26 +28,40 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send MAIL FROM command and handle server response.
     # Fill in start
+    MAILFROMCommand = "MAIL FROM: <MY EMAIL ADDRESS>\r\n"
+    clientSocket.send(MAILFROMCommand.encode())
+    recv1 = clientSocket.recv(1024).decode()
     # Fill in end
 
     # Send RCPT TO command and handle server response.
     # Fill in start
+    RCPTTOCommand = "RCPT TO: <MY EMAIL ADDRESS>\r\n"
+    clientSocket.send(RCPTTOCommand.encode())
+    recv1 = clientSocket.recv(1024).decode()
     # Fill in end
 
     # Send DATA command and handle server response.
     # Fill in start
+    DATACommand = "DATA\r\n"
+    clientSocket.send(DATACommand.encode())
+    recv1 = clientSocket.recv(1024).decode
     # Fill in end
 
     # Send message data.
     # Fill in start
+    clientSocket.send(msg)
     # Fill in end
 
     # Message ends with a single period, send message end and handle server response.
     # Fill in start
+    clientSocket.send(msg,endmsg)
     # Fill in end
 
     # Send QUIT command and handle server response.
     # Fill in start
+    QUITCommand = "QUIT\r\n"
+    clientSocket.send(QUITCommand.encode())
+    recv1 = clientSocket.recv(1024).decode
     # Fill in end
 
 
